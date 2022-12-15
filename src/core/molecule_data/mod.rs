f510d7cc14198bc461b012ed90edd60ec25fad61
JeremyMoeglich <jeremy@moeglich.dev>
2022-12-15 06:59:23 +0100

    changes

--- src/core/molecule_data/mod.rs
+++ src/core/molecule_data/mod.rs
@@ -19,11 +19,11 @@ fn get_raw_molecule_data() -> Vec<RawMoleculeData> {
 
 #[derive(Debug, Deserialize)]
 #[serde(deny_unknown_fields)]
-pub struct RawMoleculeData {
+struct RawMoleculeData {
     #[serde(rename = "@id")]
     id: String,
     #[serde(rename = "@type", default)]
-    schemas: Vec<String>,
+    _schemas: Vec<String>,
     #[serde(rename = "http://dbpedia.org/ontology/inchi", default)]
     inchi: Vec<RawMoleculeDataValue>,
     #[serde(rename = "http://dbpedia.org/ontology/molecularWeight", default)]
@@ -31,7 +31,7 @@ pub struct RawMoleculeData {
     #[serde(rename = "http://dbpedia.org/property/inchikey", default)]
     inchikey: Vec<RawMoleculeDataValue>,
     #[serde(rename = "http://www.w3.org/2000/01/rdf-schema#seeAlso", default)]
-    see_also: Vec<RawMoleculeDataId>,
+    _see_also: Vec<RawMoleculeDataId>,
     #[serde(rename = "http://www.w3.org/2004/02/skos/core#broader", default)]
     broader: Vec<RawMoleculeDataId>,
     #[serde(rename = "http://www.w3.org/2004/02/skos/core#narrower", default)]
@@ -53,7 +53,7 @@ pub struct RawMoleculeData {
     #[serde(rename = "http://www.w3.org/2004/02/skos/core#altLabel", default)]
     alt_label: Vec<RawMoleculeDataValue>,
     #[serde(rename = "http://ii.uwb.edu.pl/cvme#patent", default)]
-    patent: Vec<RawMoleculeDataId>,
+    _patent: Vec<RawMoleculeDataId>,
 }
 
 #[derive(Debug, Deserialize)]
@@ -72,16 +72,76 @@ struct RawMoleculeDataId {
     id: String,
 }
 
+#[derive(Debug)]
+pub struct MoleculeData {
+    pub id: usize,
+    pub inchi: Option<String>,
+    pub molecular_weight: Option<f64>,
+    pub inchikey: Option<String>,
+    pub broader: Vec<usize>,
+    pub narrower: Vec<usize>,
+    pub related: Vec<usize>,
+    pub example: Option<String>,
+    pub hidden_label: Option<String>,
+    pub notation_smiles: Option<String>,
+    pub definition: Option<String>,
+    pub pref_label: Option<String>,
+    pub cas_number: Option<String>,
+    pub alt_labels: Vec<String>,
+}
+
+fn extract_id(url: &str) -> usize {
+    let prefix = "CHEBI_";
+    let last_path = url.split('/').last().unwrap();
+    let id = last_path.trim_start_matches(prefix);
+    id.parse().unwrap()
+}
+
 pub fn get_molecule_data() -> Vec<MoleculeData> {
     let raw_data = get_raw_molecule_data();
     let mut data = Vec::new();
+
     for raw_molecule_data in raw_data {
-        data.push(raw_molecule_data);
+        let molecule_data = MoleculeData {
+            id: extract_id(&raw_molecule_data.id),
+            inchi: raw_molecule_data.inchi.get(0).map(|x| x.value.clone()),
+            molecular_weight: raw_molecule_data
+                .molecular_weight
+                .get(0)
+                .map(|x| x.value.parse().unwrap()),
+            inchikey: raw_molecule_data.inchikey.get(0).map(|x| x.value.clone()),
+            broader: raw_molecule_data
+                .broader
+                .iter()
+                .map(|x| extract_id(&x.id))
+                .collect(),
+            narrower: raw_molecule_data
+                .narrower
+                .iter()
+                .map(|x| extract_id(&x.id))
+                .collect(),
+            related: raw_molecule_data
+                .related
+                .iter()
+                .map(|x| extract_id(&x.id))
+                .collect(),
+            example: raw_molecule_data.example.get(0).map(|x| x.value.clone()),
+            hidden_label: raw_molecule_data.hidden_label.get(0).map(|x| x.value.clone()),
+            notation_smiles: raw_molecule_data.notation.get(0).map(|x| x.value.clone()),
+            definition: raw_molecule_data.definition.get(0).map(|x| x.value.clone()),
+            pref_label: raw_molecule_data.pref_label.get(0).map(|x| x.value.clone()),
+            cas_number: raw_molecule_data.cas_number.get(0).map(|x| x.value.clone()),
+            alt_labels: raw_molecule_data
+                .alt_label
+                .iter()
+                .map(|x| x.value.clone())
+                .collect(),
+        };
+        data.push(molecule_data);
     }
+
     data
 }
-
-type MoleculeData = RawMoleculeData; // TODO: Add more fields
 
 lazy_static! {
     pub static ref MOLECULE_DATA: Vec<MoleculeData> = get_molecule_data();
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,38 @@
-use std::mem::size_of_val;
-
 use chem::core::molecule_data::MOLECULE_DATA;
 
 fn main() {
     let data = &MOLECULE_DATA;
-    let size = size_of_val(&**data);
-    println!("Size of expr: {}", size);
-    println!("{:#?}", data.len());
+    macro_rules! stats {
+        ($name:ident) => {
+            println!(
+                "{:?} {:?}",
+                stringify!($name),
+                data.iter().filter(|x| x.$name.is_some()).count()
+            );
+        };
+    }
+    macro_rules! stats_vec {
+        ($name:ident) => {
+            println!(
+                "{:?} {:?}",
+                stringify!($name),
+                data.iter().filter(|x| !x.$name.is_empty()).count()
+            );
+        };
+    }
+    stats!(inchi);
+    stats!(molecular_weight);
+    stats!(inchikey);
+    stats_vec!(broader);
+    stats_vec!(narrower);
+    stats_vec!(related);
+    stats!(example);
+    stats!(hidden_label);
+    stats!(notation_smiles);
+    stats!(definition);
+    stats!(pref_label);
+    stats!(cas_number);
+    stats_vec!(alt_labels);
+    
+    println!(r#""total": {:?}"#, data.len());
 }
